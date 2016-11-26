e5adeb230d6227743706134ae8151bd5345eb72c
Michal Zalecki <michal@michalzalecki.com>
2016-11-26 02:11:54 +0100

    Fix Container in production

--- src/common/components/Container/Container.js
+++ src/common/components/Container/Container.js
@@ -2,6 +2,6 @@ import React from "react";
 
 const Container = process.env.NODE_ENV === "development" ?
   require("react-hot-loader").AppContainer :
-  ({ children }) => React.children.only(children);
+  ({ children }) => React.Children.only(children);
 
 export default Container;
